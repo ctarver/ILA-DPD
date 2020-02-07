ac7817e9955981a190abd8503cbc743ac30dace5
Chance Tarver <c.tarver@samsung.com>
2020-02-07 10:44:32 -0600

    Have webrf also pass original output for measurments

--- webRF.m
+++ webRF.m
@@ -21,7 +21,7 @@ classdef webRF < handle
             obj.synchronization.sub_sample = 1;
         end
         
-        function y = transmit(obj, x)
+        function [y,  y_original] = transmit(obj, x)
             %transmit. Take input signal, x, and broadcast it through the
             %RFWebLab PA.
             %
@@ -35,11 +35,11 @@ classdef webRF < handle
             if length(x) > 1000000
                 warning("Too long for webRF.");
             end
-            [y, obj.RMSout, obj.Idc, obj.Vdc] = RFWebLab_PA_meas_v1_1(x, obj.RMSin);
+            [y_original, obj.RMSout, obj.Idc, obj.Vdc] = RFWebLab_PA_meas_v1_1(x, obj.RMSin);
             
             
             % Need something to guarantee same as input length and aligned in TD.
-            y = [y(7:end)];
+            y = [y_original(7:end)];
             length_input = length(x);
             length_output = length(y);
             y = [y; zeros(length_input - length_output, 1)];
